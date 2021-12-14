34b7a9a7922acf1d785d20e31705587d1430e30b
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-13 20:21:03 -0600

    succesfully registered user to database with hashed password

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,8 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
-
+const bcrypt = require('bcryptjs')
+const User = require('../users/users-model')
 const {
   checkPasswordLength,
   checkUsernameExists,
@@ -32,7 +33,14 @@ const {
  */
 
   router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
-      res.json('register')
+      const {username, password} = req.body 
+      const hash = bcrypt.hashSync(password, 8) //number is 2 ^ 10
+
+      User.add({ username, password: hash })
+        .then(saved => {
+          res.status(201).json(saved)
+        })
+        .catch(next)
   })
 
 
