7169d1b9dc25393a44887bc5c43c132638fbcb04
Giovanni  Rosales <giovannirosales@Giovannis-MacBook-Pro.local>
2021-12-13 19:52:17 -0600

    most of middleware is built

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,6 +2,12 @@
 // middleware functions from `auth-middleware.js`. You will need them here!
 const router = require('express').Router()
 
+const {
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+} = require('./auth-middleware')
+
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -25,7 +31,7 @@ const router = require('express').Router()
   }
  */
 
-  router.post('/register', (req, res, next) => {
+  router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
       res.json('register')
   })
 
@@ -46,7 +52,7 @@ const router = require('express').Router()
   }
  */
 
-  router.post('/login', (req, res, next) => {
+  router.post('/login', checkUsernameExists, (req, res, next) => {
     res.json('login')
 })
 
